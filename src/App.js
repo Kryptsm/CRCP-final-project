d0033a5d782e22c3c6211d34638e4629886ca4be
Sam Walsh <srwalsh@smu.edu>
2021-11-24 15:18:50 -0600

    various fixes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
 import './hexagons.css';
-import { Howl, Howler } from 'howler';
+import { Howl } from 'howler';
 import Game from './Game.js';
 
-import brightbulb from "./images/brightbulb.png";
-import darkbulb from "./images/darkbulb.png";
-
-import testSound from "./sounds/testsound.mp3";
 import bubbles from "./sounds/bubbles.mp3";
 import magic from "./sounds/magic.mp3";
-import sleigh from "./sounds/sleigh.mp3";
-import mallet from "./sounds/mallet.mp3";
-import rifle from "./sounds/rifle.mp3";
-import shout from "./sounds/shout.mp3";
 
 import blaster from "./sounds/AlienBlaster.mp3";
 import radio from "./sounds/AlienRadio.mp3";
@@ -25,21 +17,19 @@ import R2D2 from "./sounds/R2D2.mp3";
 import black from "./images/buttonOff.png";
 import white from "./images/buttonOn.png";
 
-var storedSounds = [testSound, blaster, radio, ambient, borealis, mario, organ, R2D2, mallet];
+var storedSounds = [magic, blaster, radio, ambient, borealis, mario, organ, R2D2, bubbles];
 var turnOnArray = [[], [], [], [], [], [], [], [], []];
 var soundsArray = []
 var setSounds = []
 
-var doRandoms = true;
 var randomClicks = false;
-var randomInterval = 150;
 var theInterval;
 
 var currentSoundNum = 0;
 
 function App() {
   initializeRandoms();
-  initializeSounds(0);
+  initializeSounds(8);
   initializeSetSounds();
 
   return (
@@ -129,11 +119,12 @@ function App() {
 //initializes the array of sounds to the sound number specified
 export function initializeSounds(number) {
   currentSoundNum = number;
+  stopAllNoise();
   if (soundsArray.length > 0)
     soundsArray = [];
 
   for (var x = 0; x < 7; x++) {
-    if(x === 4){
+    if (x === 4) {
       soundsArray.push(
         new Howl({
           src: storedSounds[number],
@@ -142,7 +133,7 @@ export function initializeSounds(number) {
         })
       )
     }
-    else{
+    else {
       soundsArray.push(
         new Howl({
           src: storedSounds[number],
@@ -151,6 +142,12 @@ export function initializeSounds(number) {
         })
       )
     }
+  }
+}
+
+function stopAllNoise() {
+  for (var x = 0; x < soundsArray.length; x++) {
+    soundsArray[x].stop();
   }
 }
 
@@ -187,13 +184,11 @@ function initializeRandoms() {
 function handleRandomClicks() {
   if (randomClicks) {
     clearInterval(theInterval);
-    for(var x = 0; x < 7; x++){
-      soundsArray[x].stop();
-    }
+    stopAllNoise();
   } else if (!randomClicks) {
     theInterval = setInterval(function () {
       lightButton(randomIntFromInterval(1, 7));
-    }, randomInterval);
+    }, 150);
   }
   randomClicks = !randomClicks;
 }
@@ -207,12 +202,11 @@ function randomIntFromInterval(min, max) {
 function lightButton(number) {
   toggleLight(number);
 
-  if (doRandoms) {
-    var specificArray = turnOnArray[number - 1];
-    for (var x = 0; x < specificArray.length; x++) {
-      toggleLight(specificArray[x]);
-    }
+  var specificArray = turnOnArray[number - 1];
+  for (var x = 0; x < specificArray.length; x++) {
+    toggleLight(specificArray[x]);
   }
+
 }
 
 //lights the specified light number
@@ -221,21 +215,23 @@ function toggleLight(number) {
   var element = document.getElementsByClassName(elementClass)[0];
   if (element) {
     if (element.id.includes("white")) {
-      element.id="black";
+      element.id = "black";
       element.src = black;
 
-      soundsArray[number - 1].stop();
+      //soundsArray[number - 1].stop();
     }
     else if (element.id.includes("black")) {
       element.id = "white";
       element.src = white;
 
       soundsArray[number - 1].play();
-      if(currentSoundNum === 4){
-        soundsArray[number - 1].fade(1, 0.0, 3000);
+      if (currentSoundNum === 4) {
+        soundsArray[number - 1].fade(1, 0.0, 500);
       }
+      else if(currentSoundNum === 2)
+        soundsArray[number - 1].fade(0.035, 0.0, 500);
       else
-        soundsArray[number - 1].fade(0.2, 0.0, 1000);
+        soundsArray[number - 1].fade(0.2, 0.0, 500);
     }
   }
 }
